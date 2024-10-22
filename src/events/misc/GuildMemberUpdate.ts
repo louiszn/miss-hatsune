0eb22e33d2fa6d1ef26b63684d8eeb6b8f2869e0
Louis <louiszn269@gmail.com>
2024-10-22 22:26:21 +0700

    feat: auto roles

---
+++ src/events/misc/GuildMemberUpdate.ts
@@ -0,0 +1,24 @@
+import { type GuildMember } from "npm:discord.js";
+import Listener from "../Listener.ts";
+
+export default class extends Listener {
+    public constructor() {
+        super("guildMemberUpdate");
+    }
+
+    public override async execute(member: GuildMember) {
+        if (!member.flags.has("CompletedOnboarding") || !member.flags.has("BypassesVerification") || member.pending) {
+            return;
+        }
+
+        const { guild } = member;
+
+        const role = await guild.roles.fetch("1292102284599361666");
+
+        if (!role) {
+            return;
+        }
+
+        await member.roles.add(role);
+    }
+}
